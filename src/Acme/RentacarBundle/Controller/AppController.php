916c304b8097547c147d78220e3f2d74e629c7b9
root <root@novem.kernel.co.jp>
2014-07-01 16:23:54 +0900

    First commit

---
+++ src/Acme/RentacarBundle/Controller/AppController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Acme\RentacarBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * AppController.
+ *
+ * @author Ishihara Hisashi <ishihara@kernel.co.jp>
+ */
+abstract class AppController extends Controller
+{
+}
